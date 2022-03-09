6523c9b02973489771dd7b6c65a8834489e1a5b3
Antony Caseres <accasere@outlook.com>
2022-03-09 13:36:35 -0600

    Completed Assigment

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(firstValue,SecondValue){
+    return firstValue + SecondValue
+}
+
+function subtract(firstValue,SecondValue){
+    return firstValue - SecondValue
+}
+
+function multiply(firstValue,SecondValue){
+    return firstValue * SecondValue
+}
+
+function divide(firstValue,SecondValue){
+    return firstValue / SecondValue
+}
+
+function increment(n) {
+    n++
+    return n 
+}
+
+function decrement(n) {
+    n--
+    return n 
+}
+
+function makeInt(n) {
+    return parseInt(n, 10) 
+}
+
+function preserveDecimal(number){
+    return parseFloat(number)
+}
